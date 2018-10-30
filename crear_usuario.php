89ba99ec33202ea902dae5d93621b2e25e843639
kevin.humareda <kevin.humareda@usil.pe>
2018-10-30 15:44:19 -0500

    registrar usuario

---
+++ crear_usuario.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Iniciar Sesion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="usuario_procesar.php" method="post">
+        <div>
+            Nombre: <input type="text" name="name" id="name">
+        </div>
+        <div>
+            Apellido Paterno: <input type="text" name="apellidoP" id="apellidoP">
+        </div>
+        <div>
+            Apellido Materno: <input type="text" name="apellidoM" id="apellidoM">
+        </div>
+        <div>
+            DNI: <input type="text" name="dni" id="dni">
+        </div>
+        <div>
+            Fecha de Nacimiento: <input type="date" name="fecha_nacimiento" id="fecha_nacimiento">
+        </div>
+        <div>
+            Dirección: <input type="text" name="direccion" id="direccion">
+        </div>
+        <div>
+            Teléfono: <input type="text" name="telefono" id="telefono">
+        </div>
+        <div>
+            Correo Electrónico: <input type="email" name="correo" id="correo">            
+        </div>
+        <div>
+            Contraseña: <input type="password" name="contraseña" id="contraseña">
+        </div>
+        <div>
+            Confirmar Contraseña: <input type="password" name="confirmar" id="confirmar">
+        </div>
+        <button type="submit">Registrar</button>
+    </form>
+    
+</body>
+</html>
