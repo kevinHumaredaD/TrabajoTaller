3d3e18fbfd1301edc1cd3ae32dce508912837337
Angello Cruz <angellocruz0911@gmail.com>
2018-10-30 15:42:57 -0500

    login

---
+++ partes/conexion.php
@@ -0,0 +1,3 @@
+<?php
+    $db= new PDO('mysql:host=localhost;dbname=keanStore;charset=utf8mb4','root','');
+?>
---
+++ partes/header.php
@@ -0,0 +1,11 @@
+<nav>
+    <a href="index.php">Portada</a>
+    <a href="mostrar_productos.php">Productos registrados</a>
+    <a href="">Vendedores destacados</a>
+    <a href="">Crear cuenta de usuario</a>
+    <?php if(isset($_SESSION["correo"])) {?>
+        <a href="logout.php">Cerrar Sesion</a>
+    <?php }else{ ?>
+        <a href="login.php">Iniciar sesion</a>
+    <?php } ?>
+</nav>
