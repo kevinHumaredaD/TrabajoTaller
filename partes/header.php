2ed33526b14b685c8fc46df69eeed57d146ca6b8
Angello Cruz <angellocruz0911@gmail.com>
2018-11-06 20:58:48 -0500

    Actualizacion de partes

--- partes/header.php
+++ partes/header.php
@@ -1,19 +1,13 @@
 <nav>
     <a href="index.php">Portada</a>
-    <a href="mostrar_productos.php">Productos registrados</a>
-    <a href="">Vendedores destacados</a>
-    <?php if(!isset($_SESSION["correo"])) {?>
-        <a href="crear_usuario.php">Crear cuenta de usuario</a>
-    <?php }?>
+    
+    <a href="vendedores_destacados.php">Vendedores destacados</a>
     <?php if(isset($_SESSION["correo"])) {?>
-        <a href="logout.php">Cerrar Sesion</a>
-<<<<<<< HEAD
+        <a href="mostrar_productos.php">Mis Productos</a>
         <a href="libro_reclamo.php">Libro de Reclamo</a>
-=======
         <a href="registrar_producto.php">Registrar producto</a>
->>>>>>> aa0cdaab4002ca0bb654edc99fe4cce92daa0714
     <?php }else{ ?>
-        <a href="login.php">Iniciar sesion</a>
+        <a href="crear_usuario.php">Crear cuenta</a>
     <?php } ?>
     
 </nav>
